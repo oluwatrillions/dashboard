97bcd6336b3777de53c90b26a223e851821719e2
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2023-05-12 18:04:08 +0100

    bar chart

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+import BarChart from './components/BarChart'
+import { userData } from './Data'
+import {Chart as ChartJS} from 'chart.js/auto'
 
 function App() {
 
+    console.log(userData.age);
+
+    const [scoredata, setScoredata] = useState({
+        labels: userData.map((age) => age.age),
+
+        datasets: [
+            {
+                label: 'Average score of students by age and sex',
+                data: userData.map((students) => students.averageScore)
+            }
+        ]
+    })
+
   return (
-    <>
-        <div>
-            <h3>Dashboard</h3>  
-      </div>
-    </>
+    <div>
+          <BarChart score={ scoredata } />  
+    </div>
   )
 }
 
---
+++ dashboard/src/Data.jsx
@@ -0,0 +1,26 @@
+export const userData = [
+    {
+        id: 1,
+        age: '11 - 16',
+        sex: "male",
+        averageScore : 55
+    },
+    {
+        id: 2,
+        age: '17 - 21',
+        sex: "female",
+        averageScore : 75
+    },
+    {
+        id: 3,
+        age: '22 - 26',
+        sex: "female",
+        averageScore : 65
+    },
+    {
+        id: 4,
+        age: '27 - 32',
+        sex: "male",
+        averageScore : 85
+    },
+]
